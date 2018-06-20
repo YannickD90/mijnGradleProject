f3a6adaff7940882c7e7b77e48ef02581e59ac59
vdabcursist <decock.yannick@hotmail.com>
2018-06-20 09:02:57 +0200

    test5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@ public class Main {
 
 
         System.out.println("zever");
+        System.out.println("nog meer test zever");
 
     }
 }
---
+++ src/main/java/Testclass2.java
@@ -0,0 +1,4 @@
+public class Testclass2 {
+
+    //dit is nog een test classe
+}
